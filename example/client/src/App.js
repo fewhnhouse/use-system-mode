2775607b064e9cc6fcacd7b68dc10a14eb5f7564
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-03-21 15:51:08 +0100

    improve example.

--- example/client/src/App.js
+++ example/client/src/App.js
@@ -4,11 +4,10 @@ import { useSystemMode, useIsDarkMode } from '@fe.whnhouse/use-system-mode'
 const App = () => {
   const mode = useSystemMode()
   const darkMode = useIsDarkMode()
-  console.log(mode)
   return (
-    <div>
-      <h1>System Mode:{mode}</h1>
-      <h2>Dark Mode: {darkMode ? 'On' : 'Off'}</h2>
+    <div style={{ width: '100vw', height: '100vh', background: darkMode ? '#2f3542' : 'white', padding: 20 }}>
+      <h1 style={{ color: darkMode ? 'white' : 'black' }}>System Mode: {mode}</h1>
+      <h2 style={{ color: darkMode ? 'white' : 'black' }}>Dark Mode: {darkMode ? 'On' : 'Off'}</h2>
     </div>
   )
 }
--- example/typescript-client/src/App.tsx
+++ example/typescript-client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useSystemMode } from '@fe.whnhouse/use-system-mode'
+import { useSystemMode, useIsDarkMode } from '@fe.whnhouse/use-system-mode'
 
 const App = () => {
   const mode = useSystemMode()
-  console.log(mode)
+  const darkMode = useIsDarkMode()
   return (
-    <div>
-      <h1>System Mode:{mode}</h1>
+    <div style={{ width: '100vw', height: '100vh', background: darkMode ? '#2f3542' : 'white', padding: 20 }}>
+      <h1 style={{ color: darkMode ? 'white' : 'black' }}>System Mode: {mode}</h1>
+      <h2 style={{ color: darkMode ? 'white' : 'black' }}>Dark Mode: {darkMode ? 'On' : 'Off'}</h2>
     </div>
   )
 }
